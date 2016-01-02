b1551ac05d09431dfe6f42e425841b1cb3346fb9
t00n <kelkununtel@hotmail.com>
2016-01-02 16:09:08 +0100

    [ref] dataloader geometries

--- dataloader.py
+++ dataloader.py
@@ -1,22 +1,20 @@
 from collada import *
+from collada.scene import GeometryNode
 
 data = Collada('data/chessboard+man.dae')
 
 class Geometry:
-    pass
+    def __init__(self, geo):
+        self.vertices = []
+        self.normals = []
+        for prim in geo.primitives:
+            self.vertices.extend(prim.vertex[prim.vertex_index][:,[0,2,1]])
+            self.normals.extend(prim.normal[prim.normal_index][:,[0,2,1]])
 
 
 geometries = {}
 for i, node in enumerate(data.scene.nodes):
-    try:
-        name = "".join(node.id.split('_')[:2])
-        geo = node.children[0].geometry
-        my_geo = Geometry()
-        my_geo.vertices = []
-        my_geo.normals = []
-        for prim in geo.primitives:
-            my_geo.vertices.extend(prim.vertex[prim.vertex_index][:,[0,2,1]])
-            my_geo.normals.extend(prim.normal[prim.normal_index][:,[0,2,1]])
-        geometries[name] = my_geo
-    except AttributeError:
-        pass
+    for child in node.children:
+        if isinstance(child, GeometryNode):
+            name = "".join(node.id.split('_')[:2])
+            geometries[name] = Geometry(child.geometry)
