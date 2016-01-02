7dd5d6323d9c94293356ec4eab51b0b26d8ab9e4
t00n <kelkununtel@hotmail.com>
2016-01-02 17:19:45 +0100

    [add] load material and effects

--- dataloader.py
+++ dataloader.py
@@ -3,6 +3,10 @@ from collada.scene import GeometryNode
 
 data = Collada('data/chessboard+man.dae')
 
+class Material:
+    def __init__(self, mat):
+        self.effect = get_effect(mat.effect.id)
+        
 class Geometry:
     def __init__(self, geo):
         self.vertices = []
@@ -10,7 +14,19 @@ class Geometry:
         for prim in geo.primitives:
             self.vertices.extend(prim.vertex[prim.vertex_index][:,[0,2,1]])
             self.normals.extend(prim.normal[prim.normal_index][:,[0,2,1]])
+            self.material = Material(get_material(prim.material))
 
+def get_material(symbol):
+    for mat in data.materials:
+        if mat.id == symbol:
+            return mat
+    return None
+
+def get_effect(id):
+    for effect in data.effects:
+        if effect.id == id:
+            return effect
+    return None
 
 geometries = {}
 for i, node in enumerate(data.scene.nodes):
