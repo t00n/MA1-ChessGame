393242537d003153a6907448c4377d892474e5fe
t00n <kelkununtel@hotmail.com>
2016-01-04 16:54:18 +0100

    [add] fresnel schlick

--- shaders/obj.fs
+++ shaders/obj.fs
@@ -15,8 +15,19 @@ uniform vec3 u_light_position;
 uniform vec4 u_light_intensities;
 uniform vec3 u_camera_position;
 
-vec3 half_vector(vec3 v1, vec3, v2) {
+float R0 = pow((1 - u_index_of_refraction)/(1 + u_index_of_refraction), 2);
+
+vec3 half_vector(vec3 v1, vec3 v2) {
     return (v1 + v2) / (length(v1 + v2));
+}
+
+float F_schlick(vec3 from_light, vec3 hv) {
+    return R0 + (1 - R0) * pow(1 - dot(from_light, hv), 5);
+}
+
+float cook_torrance(vec3 to_light, vec3 to_camera, vec3 normal, float roughness) {
+    vec3 hv = half_vector(to_light, to_camera);
+    return F_schlick(-to_light, hv);
 }
 
 void main(void) {
@@ -26,17 +37,14 @@ void main(void) {
     vec3 position = vec3(u_model * vec4(v_position, 1));
 
     // Diffuse component
-    vec3 frag_to_light = normalize(u_light_position - position);
-    float diffuse_coef = dot(normal, frag_to_light);
+    vec3 to_light = normalize(u_light_position - position);
+    float diffuse_coef = dot(normal, to_light);
     diffuse_coef = clamp(diffuse_coef, 0, 1);
     vec4 diffuse = diffuse_coef * u_diffuse * u_light_intensities;
 
     // Specular component
-    vec3 incidence_vector = -frag_to_light;
-    vec3 frag_to_camera = normalize(u_camera_position - position);
-    vec3 reflection_vector = reflect(incidence_vector, normal);
-    float reflection_angle = dot(frag_to_camera, reflection_vector);
-    float specular_exponent = pow(reflection_angle, u_shininess);
+    vec3 to_camera = normalize(u_camera_position - position);
+    float specular_exponent = cook_torrance(to_light, to_camera, normal, 0.3);
     vec4 specular = specular_exponent * u_specular * u_light_intensities;
 
     // Ambient component
