[fix] scaling, rotation, translation

gl.py
@@ -153,7 +153,7 @@ class GLWidget(QGLWidget):
         self.main_program = MainProgram()
         self.texture_program = TextureProgram()
 
-        self.animations = []
+        self.animations = [Animation(self.board.board[0][0], [7,0])]
 
         QTimer.singleShot(0, self.update)
 
@@ -224,12 +224,12 @@ class GLWidget(QGLWidget):
         return look_at(self.camera.position, self.camera.direction, (0,1,0))
 
     def _model_matrix(self, geo, position):
-        return reduce(np.dot, [translate([(position[0])*6, 0, (position[1])*6]),
-                                    translate(geo.translation),
-                                    rotate(geo.rotation[2], [0, 0, 1]),
-                                    rotate(geo.rotation[1], [0, 1, 0]),
-                                    rotate(geo.rotation[0], [1, 0, 0]),
-                                    scale(geo.scaling)])
+        return reduce(np.dot, [scale(geo.scaling),
+                               rotate(geo.rotation[0], [1, 0, 0]),
+                               rotate(geo.rotation[1], [0, 1, 0]),
+                               rotate(geo.rotation[2], [0, 0, 1]),
+                               translate(geo.translation),
+                               translate([(position[0])*4, 0, (position[1])*4])])
 
     def create_fbo(self):
         fbo = glGenFramebuffers(1)

gl_data.py
@@ -1,33 +1,22 @@
 from collada import *
 from collada.scene import GeometryNode
 import numpy as np
-
-def get_material(symbol):
-    for mat in data.materials:
-        if mat.id == symbol:
-            return mat
-    return None
-
-def get_effect(id):
-    for effect in data.effects:
-        if effect.id == id:
-            return effect
-    return None
+# from vispy.util.transforms import *
 
 class Material:
     def __init__(self, mat):
-        self.effect = get_effect(mat.effect.id)
+        self.effect = mat.effect
         
 class Geometry:
     def __init__(self, geo):
         self.vertices = []
         self.normals = []
         self.materials = []
-        for prim in geo.primitives:
+        for prim in geo.primitives():
             self.vertices.append(prim.vertex[prim.vertex_index][:,[0,2,1]])
             self.normals.append(prim.normal[prim.normal_index][:,[0,2,1]])
-            self.materials.append(Material(get_material(prim.material)))
-        self.translation = np.array([-21, 0, -18], dtype=np.float32) # translate to world origin
+            self.materials.append(Material(prim.material))
+        self.translation = np.array([0, 0, 0], dtype=np.float32) # translate to world origin
         self.rotation = np.array([0, 0, 0], dtype=np.float32)
         self.scaling = np.array([1, 1, 1], dtype=np.float32)
 
@@ -35,10 +24,28 @@ class Geometry:
 data = Collada('data/chessboard+man.dae')
 geometries = {}
 for i, node in enumerate(data.scene.nodes):
-    for child in node.children:
-        if isinstance(child, GeometryNode):
-            name = "".join(node.id.split('_')[:2])
-            geometries[name] = Geometry(child.geometry)
+    for geo in node.objects('geometry'):
+        name = "".join(node.id.split('_')[:2])
+        geometries[name] = Geometry(geo)
 
 # Adjust some specific things
-geometries['Chessboard'].translation = np.array([0, -15, 0])
+geometries['Chessboard'].scaling = np.array([1.5, 1, 1.5])
+for key, geo in geometries.items():
+    if key != 'Chessboard':
+        geo.translation = np.array([-14,0,-13.5])
+# print(geometries['Chessboard'].vertices)
+# geometries['Chessboard'].vertices = np.apply_along_axis(lambda x: [x[0], x[1], x[2]], 2, geometries['Chessboard'].vertices)
+# geometries['WhiteRook'].scaling = np.array([0.4,0.4,0.4])
+# geometries['BlackRook'].scaling = np.array([0.4,0.4,0.4])
+# geometries['WhiteKnight'].rotation = np.array([0,90,0])
+# geometries['BlackKnight'].rotation = np.array([0,-90,0])
+# geometries['WhiteKnight'].scaling = np.array([1.0, 0.8, 1.0])
+# geometries['BlackKnight'].scaling = np.array([1.0, 0.8, 1.0])
+# geometries['WhiteKing'].scaling = np.array([1.0, 1.4, 1.0])
+# geometries['BlackKing'].scaling = np.array([1.0, 1.4, 1.0])
+# geometries['WhiteQueen'].scaling = np.array([1.0, 1.2, 1.0])
+# geometries['BlackQueen'].scaling = np.array([1.0, 1.2, 1.0])
+# geometries['WhitePawn'].scaling = np.array([0.8, 0.8, 0.8])
+# geometries['BlackPawn'].scaling = np.array([0.8, 0.8, 0.8])
+# geometries['WhiteBishop'].scaling = np.array([1.0, 1.4, 1.0])
+# geometries['BlackBisàhop'].scaling = np.array([1.0, 1.4, 1.0])